09698023cc47e84586207e9c59ce44e3b06a24c6
ScottHiscock <scott.hiscock@email.cz>
2014-11-30 13:46:32 +0100

    FirstCommit

---
+++ Models/SubCellCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace WebSubCells.Models
+{
+    public class SubCellCode
+    {
+        public int ID { get; set; }
+        public int SubCellHierarchyID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class SubCellCodeDBContext : DbContext
+    {
+        public DbSet<SubCellCode> SubCellCode { get; set; }
+    }
+}
---
+++ Models/SubCellHierarchy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace WebSubCells.Models
+{
+    public class SubCellHierarchy
+    {
+        public int ID { get; set; }
+        public int ParentID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Order { get; set; }
+    }
+
+    public class SubCellHierarchyDBContext : DbContext
+    {
+        public DbSet<SubCellHierarchy> SubCellHierarchy { get; set; }
+    }
+}
